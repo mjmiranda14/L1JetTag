f16688546d9f9c1fa12c39a2d94d2904444a90d3
simon-poon <93788837+simon-poon@users.noreply.github.com>
2022-10-20 05:15:28 -0400

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -1,68 +1,119 @@
 import h5py
-import optparse
+import awkward as ak
+import uproot
+import argparse
 import numpy as np
 import pathlib
 import matplotlib.pyplot as plt
 import mplhep as hep
 
-stop_samp = 'signalPartsDatasamp.h5'
-stop_bkg = 'missedSignalPartsDatasamp.h5'
-QCD_bkg = 'jetDatabkg.h5'
-dset = [(stop_samp, 'r', 'stop samp'), (stop_bkg, 'g', 'stop bkg'), (QCD_bkg, 'b', 'QCD bkg')]
-for idx, feat in enumerate(['pt', 'eta', 'phi', 'm']):
-    for i, d in enumerate(dset):
-        h5file_path = d[0]
-        array_name = 'Data'
-        if i == 2:
-            array_name = 'Jet ' + array_name
-        with h5py.File(h5file_path, 'r') as h5f:
-            print(nph5f[array_name])
+parser = argparse.ArgumentParser()
+parser.add_argument('--QCD-part', action='store', type=str, required=False, help='file path for QCD file particle level features')
+parser.add_argument('--stop-part', action='store', type=str, required=False, default=100, help='file path for stop file particle level features')
+parser.add_argument('--QCD-bkg', action='store', type=str, required=False, help='file path for QCD background')
+parser.add_argument('--stop-samp', action='store', type=str, required=False, help='file path for stop file sample')
+parser.add_argument('--stop-bkg', action='store', type=str, required=False, help='file path for stop file background')
+parser.add_argument('--output', action='store', type=str, required=True, help='output path')
+args = parser.parse_args()
+
+output_path = args.output    #"C:\\Users\\eagle\\Research\\commit\\L1METML\\L1JetTag\\"
+output_path = output_path + 'graph.root'
+
+create_file = uproot.writing.writable.recreate(output_path)
+#with uproot.open(output_path) as file:
+mktree_dict = {}
+file_list = []
+if args.QCD_part == True:   
+    QCD_tree = {"QCD_dz": "var * float64",
+                "QCD_dx": "var * float64",
+                "QCD_dy": "var * float64",
+                "QCD_pt": "var * float64",
+                "QCD_eta": "var * float64",
+                "QCD_phi": "var * float64"}
+    mktree_dict = {**mktree_dict, **QCD_tree}
+    file_list.append('QCD')
+if args. stop_part == True:
+    stop_dict = {"stop_dz": "var * float64",
+                 "stop_dx": "var * float64",
+                 "stop_dy": "var * float64",
+                 "stop_pt": "var * float64",
+                 "stop_eta": "var * float64",
+                 "stop_phi": "var * float64"}
+    mktree_dict = {**mktree_dict, **stop_dict}
+    file_list.append('stop')
+
+create_file.mktree('particle_level', mktree_dict)
+create_file['particle_level'].show()
+
+for kind in file_list:
+    for feat in [('dz',8), ('dx', 9), ('dy', 10), ('pt',11), ('eta', 12), ('phi', 13)]:
+        n_bins_conc = ak.Array([])
+        if kind == "QCD":
+            h5file = args.QCD_part #"trainingDatabkg.h5"
+            array_name = "Training Data"
+        elif kind == "stop":
+            h5file = args.stop_part #"trainingDatabkg.h5"
+            array_name = "Training Data"
+        with h5py.File(h5file, 'r') as h5f:
+            feat_array = h5f[array_name][:,feat[1]::14]
+            n, bins = np.histogram(feat_array, bins=100)
+            bin_width = bins[1] - bins[0]
+            weights = 1/np.sum(n*bin_width)
+            n = n*weights
+        n_bins = ak.Array([n, bins])
+        n_bins_conc = ak.concatenate((n_bins_conc, n_bins),axis=0)
+        uproot_dict[kind + "_" + feat[0]] = n_bins
+print(uproot_dict.keys())
+create_file["particle_level"].extend(uproot_dict)        
+        
+
+mktree_dict = {}
+file_list = []
+if args.QCD_bkg == True:
+    QCD_bkg_dict = {"bkg_QCD_pt": "var * float64",
+                    "bkg_QCD_eta": "var * float64",
+                    "bkg_QCD_phi": "var * float64",
+                    "bkg_QCD_m": "var * float64"}
+    mktree_dict = {**mktree_dict, **QCD_bkg_dict}
+    file_list.append("bkg_QCD")
+if args.stop_bkg == True:
+    stop_bkg_dict = {"bkg_stop_pt": "var * float64",
+                     "bkg_stop_eta": "var * float64",
+                     "bkg_stop_phi": "var * float64",
+                     "bkg_stop_m": "var * float64"}
+    mktree_dict = {**mktree_dict, **stop_bkg_dict}
+    file_list.append("bkg_stop")
+if args.stop_samp == True:
+    stop_samp_dict = {"samp_stop_pt": "var * float64",
+                      "samp_stop_eta": "var * float64",
+                      "samp_stop_phi": "var * float64",
+                      "samp_stop_m": "var * float64"}
+    mktree_dict = {**mktree_dict, **stop_samp_dict}
+    file_list.append("samp_stop")
+    
+create_file.mktree('jet_level', mktree_dict)
+create_file['jet_level'].show()
+
+uproot_dict = {}
+for kind in file_list:
+    for idx, feat in enumerate(['pt', 'eta', 'phi', 'm']):
+        n_bins_conc = ak.Array([])
+        if kind == "bkg_QCD":
+            h5file = args.QCD_bkg #"missedSignalPartsDatasamp.h5"
+            array_name = "Data"
+        elif kind == "bkg_stop":
+            h5file = args.stop_bkg #"missedSignalPartsDatasamp.h5"
+            array_name = "Data"
+        elif kind == "samp_stop":
+            h5file = args.stop_samp #"signalPartsDatasamp.h5"
+            array_name = "Data"
+        with h5py.File(h5file, 'r') as h5f:
             feat_array = h5f[array_name][:,idx]
             n, bins = np.histogram(feat_array, bins=100)
             bin_width = bins[1] - bins[0]
             weights = 1/np.sum(n*bin_width)
             n = n*weights
-            plt.stairs(n, edges=bins, fill=True, alpha=0.5, label=d[2]) # density=False, histtype='stepfilled', alpha=0.5, facecolor=d[1], label=d[2])
-    plt.xlabel(feat)
-    plt.xlabel(feat)
-    plt.ylabel('normalized count')
-    plt.legend(loc='upper right')
-    plt.savefig(feat)
-    plt.close()
-
-    
-output_path = args.parse
-stop = 'trainingDatasamp.h5'
-QCD = 'trainingDatabkg.h5'
-create_file = uproot.recreate(output_path)
-with uproot.open(create_file) as file:
-    file['particle_level'] = uproot.newtree({"dz": np.float64,
-                                             "dx": np.float64,
-                                             "dy": np.float64,
-                                             "pt": np.float64,
-                                             "eta": np.float64,
-                                             "phi": np.float64})
-    dset = [(stop, 'r', 'stop'), (QCD, 'b', 'QCD')]
-    uproot_dict = {}
-    for feat in [('dz',8), ('dx', 9), ('dy', 10), ('pt',11), ('eta', 12), ('phi', 13)]:
-        plt.figure(figsize=(10, 8))
-        for i, d in enumerate(dset):
-            h5file_path = d[0]
-            array_name = "Training Data"
-            #if i == 2:
-            #    array_name = 'Jet ' + array_name
-            with h5py.File(h5file_path, 'r') as h5f:
-                feat_array = h5f[array_name][:,feat[1]::14]
-                n, bins = np.histogram(feat_array, bins=100)
-                bin_width = bins[1] - bins[0]
-                weights = 1/np.sum(n*bin_width)
-                n = n*weights
-                n_bins = ak.Array([n,bins])
-                uproot_dict[feat[0]] = n_bins
-                plt.stairs(n, edges=bins, fill=True, alpha=0.5, label=d[2]) # density=False, histtype='stepfilled', alpha=0.5, facecolor=d[1], label=d[2])
-        file['particle_level'].extend(uproot_dict)
-        plt.xlabel(feat[0])
-        plt.ylabel('normalized count')
-        plt.legend(loc='upper right')
-        plt.savefig(feat[0] + '_particle_level')
-        plt.close()
+        n_bins = ak.Array([n, bins])
+        n_bins_conc = ak.concatenate((n_bins_conc, n_bins),axis=0)
+        uproot_dict[kind + "_" + feat] = n_bins
+create_file["jet_level"].extend(uproot_dict)   
